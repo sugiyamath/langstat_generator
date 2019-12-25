30830647347c4e169469f3d70d2c6698acf20b01
root <shun.sugiyama@jp21.com>
2019-12-25 02:31:59 +0000

    updated

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 08:00:00
 
 for i in $(seq 0 6); do
-    srun -N 1 -n 1 time bash execute.sh ./wet.paths.gz ${i} 3 ./out ./tmp &
+    srun -N 1 -n 1 time bash execute.sh ./wet.paths.gz ${i} 3 ./out ./tmp ./venv/bin/python &
 done
 
 wait
