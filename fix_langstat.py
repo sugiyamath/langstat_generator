5ed4e8d1fffa50a3a96c480304bda659f33a6157
root <shun.sugiyama@jp21.com>
2019-12-25 00:29:25 +0000

    fixed

--- fix_langstat.py
+++ fix_langstat.py
@@ -2,16 +2,19 @@
 def fix(lines):
     prev = ""
     prev_value = 0
+    count = 1
     for line in lines:
         tmp = line.strip().split("\t")
         current = '\t'.join(tmp[:2])
         current_value = int(tmp[2])
         if prev == current:
             prev_value += current_value
+            count += 1
             continue
         else:
             if prev and prev_value > 0:
-                yield prev+"\t"+str(prev_value)
+                yield prev+"\t"+str(float(prev_value)/float(count))
+            count = 1
         prev = current
         prev_value = current_value
     yield prev+'\t'+str(prev_value)
@@ -19,6 +22,6 @@ def fix(lines):
 
 if __name__ == "__main__":
     import sys
-    lines = (x for x in sys.stdin) # this is sorted
+    lines = (x for x in sys.stdin)
     for x in fix(lines):
         sys.stdout.write(x+"\n")
