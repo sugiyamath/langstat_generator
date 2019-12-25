2c139bcbb7ed9eb7158c9c17661e9d57d5f93d13
root <shun.sugiyama@jp21.com>
2019-12-25 23:06:58 +0000

    updated

--- execute.sh
+++ execute.sh
@@ -4,14 +4,15 @@ WET_PATH=$1
 NODE_ID=$2
 SHARD_NUM=$3
 OUT_PATH=$4
-TMP_PATH=$5
+TMP_PREFIX=$5
 PY_PATH=$6
 
 mkdir -p ${OUT_PATH}
 
 for i in $(seq 0 ${SHARD_NUM}); do
+    TMP_PATH=${TMP_PREFIX}_${NODE_ID}_${i}
     mkdir -p ${TMP_PATH}
     zcat ${WET_PATH} | head -n 1120 | ${PY_PATH} sharding.py ${NODE_ID} ${i} | awk '{print "https://commoncrawl.s3.amazonaws.com/"$0}' | head -n 50 | ./parallel -j 50 wget {} -P ${TMP_PATH}
-    ls -1d ${TMP_PATH}/* | head -n 50 | ${PY_PATH} main.py ./bin ${OUT_PATH}/lm_scores_${NODE_ID}_${i}.txt ${OUT_PATH}/langstats_${NODE_ID}_${i}.txt
+    ls -1d ${TMP_PATH}/* | head -n 50 | ${PY_PATH} main.py ./bin ${OUT_PATH}/lm_scores_${NODE_ID}_${i}.txt ${OUT_PATH}/langstats_${NODE_ID}_${i}.txt ${TMP_PATH}
     rm -r ${TMP_PATH}
     
--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def main(bin_dir, score_outpath, langstat_outpath, tmp_dir="./tmp"):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
