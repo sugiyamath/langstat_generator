cd4b8cf006119951072dfa9f0f5ad44190743ffe
root <shun.sugiyama@jp21.com>
2019-12-26 06:05:49 +0000

    update

--- execute.sh
+++ execute.sh
@@ -7,7 +7,6 @@ SHARD_ID=$4
 OUT_PATH=$5
 TMP_PATH=$6
 
-bash download.sh ${WET_PATH} ${PY_PATH} ${NODE_ID} ${SHARD_ID} ${TMP_PATH}
-bash langstat.sh ${TMP_PATH} ${PY_PATH} ${OUT_PATH} ${NODE_ID} ${SHARD_ID}
 
+zcat ${WET_PATH} | head -n 1120 | ${PY_PATH} sharding.py ${NODE_ID} ${SHARD_ID} | awk '{print "https://commoncrawl.s3.amazonaws.com/"$0}' | head -n 50 | xargs -n1 -P 50 wget {} -P ${TMP_PATH}; ls -1d ${TMP_PATH}/* | head -n 50 | ${PY_PATH} main.py ./bin ${OUT_PATH}/lm_scores_${NODE_ID}_${SHARD_ID}.txt ${OUT_PATH}/langstats_${NODE_ID}_${SHARD_ID}.txt ${TMP_PATH}
 
