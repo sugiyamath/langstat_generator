1abde46e589ac45fed488cc8048325ecdfb00595
root <shun.sugiyama@jp21.com>
2019-12-27 01:39:00 +0000

    update

--- data_downloader.py
+++ data_downloader.py
@@ -10,18 +10,22 @@ DEFAULT_CPUS = os.cpu_count()
 def _download_file(url, tmp_dir):
     local_filename = url.split('/')[-1]
     filepath = os.path.join(tmp_dir, local_filename)
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(filepath, 'wb') as f:
-            for chunk in tqdm(r.iter_content(chunk_size=8192)):
-                if chunk:
-                    f.write(chunk)
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(filepath, 'wb') as f:
+                for chunk in tqdm(r.iter_content(chunk_size=8192)):
+                    if chunk:
+                        f.write(chunk)
+    except Exception as e:
+        print(e)
+        return None
     return filepath
 
 
 def download_bulk(urls, tmp_dir, num_cpus=DEFAULT_CPUS):
     pool = Pool(num_cpus)
     func = partial(_download_file, tmp_dir=tmp_dir)
-    files = pool.map(func, urls)
+    files = [x for x in pool.map(func, urls) if x is not None]
     pool.close()
     return files
