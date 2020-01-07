58b56797fa1da55cfabc4f373a154cb29d3b3c28
root <shun.sugiyama@jp21.com>
2020-01-07 02:40:10 +0000

    updated

--- execute.sh
+++ execute.sh
@@ -7,5 +7,6 @@ MAX_SHARD_NUM=$4
 OUT_DIR=$5
 BIN_DIR=$6
 
-zcat ${WET_PATH} | head -n 1120 | awk '{print "https://commoncrawl.s3.amazonaws.com/"$0}' | ${PY_PATH} main.py ${NODE_ID} ${MAX_SHARD_NUM} ${BIN_DIR} ${OUT_DIR}
+#zcat ${WET_PATH} | head -n 1120 | awk '{print "https://commoncrawl.s3.amazonaws.com/"$0}' | ${PY_PATH} main.py ${NODE_ID} ${MAX_SHARD_NUM} ${BIN_DIR} ${OUT_DIR}
+zcat ${WET_PATH} | head -n 1120 | awk '{print "./data/"$0}' | ${PY_PATH} main.py ${NODE_ID} ${MAX_SHARD_NUM} ${BIN_DIR} ${OUT_DIR}
 
