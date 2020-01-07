e53597b915102fa9382b26a035107fa3a96bce1d
root <shun.sugiyama@jp21.com>
2020-01-07 04:22:41 +0000

    update

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def main(node_id,
     for shard_id in range(max_shard_num):
         _plog("start shard", node_id, shard_id, st)
         with tempfile.TemporaryDirectory(dir=".") as tmp_dir:
+            #targets = all_targets
             targets = list(
                 sharding.sharding(all_targets, node_id, shard_id, total_nodes,
                                   wet_per_shard))
