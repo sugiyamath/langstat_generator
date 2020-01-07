43a936a117501fe750761984f50380a9e8b9cf7f
root <shun.sugiyama@jp21.com>
2020-01-07 06:01:25 +0000

    update

--- cmd_example.sh
+++ cmd_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls -1d data_dl/* | python3 dedup.py > langstat.0.txt
-sort -t$'\t' -k1,1 -k2,2 -u langstat.0.txt | python3 fix_langstat.py > langstat.0.txt.fixed
+zcat wet.paths.gz | head -n 1120 | python3 main.py 0 1 bin/ out > log
+sort -t$'\t' -k1,1 -k2,2 -u out/langstat.0_0.txt | python3 fix_langstat.py > langstat.0_0.txt.fixed
